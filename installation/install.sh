cb345a3f22a331ba8edbf56a5cc80e5a883c3d3e
Yves Schutz <schutz@sbgmc137.in2p3.fr>
2017-04-20 15:15:00 +0200

    cleanup and new info in installation

--- installation/install.sh
+++ installation/install.sh
@@ -2,23 +2,26 @@
 # Download
 if [ "x$(uname -s)" = "xLinux" ]; then
   curl -o cernvm-launch https://ecsft.cern.ch/dist/cernvm/launch/bin/Linux/cernvm-launch
-else
+elif [ "x$(uname -s)" = "xDarwin" ]; then
   curl -o cernvm-launch https://ecsft.cern.ch/dist/cernvm/launch/bin/Mac/cernvm-launch
-fi
+fi 
 chmod +x cernvm-launch  # make it executable
 # As root user, place it in a directory from the $PATH environment
 #sudo mv        cernvm-launch /usr/local/bin
 
 # Run
 export name=`echo MCat$HOSTNAME | cut -d. -f1`
-./cernvm-launch create --name $name USER_DATA_FILE CONFIGURATION_FILE
+if ./cernvm-launch create --name $name USER_DATA_FILE CONFIGURATION_FILE; then 
+	if [ "x$(uname -s)" = "xDarwin" ]; then
+		/Applications/Firefox.app/Contents/MacOS/firefox https://cernvm-online.cern.ch/dashboard/ &
+	else
+		firefox  https://cernvm-online.cern.ch/dashboard/ &
+	fi
+	echo ========================================================================
+	echo pair the context definition MasterClass with the virtual machine MasterClass
+	echo login to your virtual machine as user name #<PIN Code>
+	echo ========================================================================
+else 
+	./cernvm-launch start $name
+fi	
 rm cernvm-launch
-if [ "x$(uname -s)" = "xDarwin" ]; then
-  /Applications/Firefox.app/Contents/MacOS/firefox https://cernvm-online.cern.ch/dashboard/ &
-else
-  firefox  https://cernvm-online.cern.ch/dashboard/ &
-fi
-echo ========================================================================
-echo pair the context definition MasterClass with the virtual machine MasterClass
-echo login to your virtual machine as user name #<PIN Code>
-echo ========================================================================
--- start.sh
+++ start.sh
@@ -48,7 +48,11 @@ export DATAPATH="$PWD/data/"
 export IMPATH="$DATAPATH/images/"
 export EVTPATH="$DATAPATH/events/$EXPATH"
 export GEOPATH="$DATAPATH/geometry/"
-. /Applications/root_v6.08.06/bin/thisroot.sh
+if [ "x$(uname -s)" = "xDarwin" ]; then
+ . /Applications/root_v6.08.06/bin/thisroot.sh
+elif [ "x$(uname -s)" = "xLinux" ]; then
+ export PATH=/cvmfs/sft.cern.ch/lcg/releases/gcc/4.8.4/x86_64-slc6/bin/:$PATH	
+ source /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/6.06-08/x86_64-slc6-gcc48-opt/root/bin/thisroot.sh	
 echo $PART
 if [[ $PART = "Part1/" ]]; then 
 	root start.C
