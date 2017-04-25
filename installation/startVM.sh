0a6fe8f1fb1c1435a63ec6775b576a3b20c19de4
yschutz <schutz@in2p3.fr>
2017-04-25 11:35:43 +0200

    improving logic

--- installation/startVM.sh
+++ installation/startVM.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
-if ! virtualbox; then
+if ! which virtualbox > /dev/null; then
  echo "you need to instal VirtualBox first from: https://www.virtualbox.org"
-if [ "x$(uname -s)" = "xLinux" ]; then
-  echo "you need to download and install VMBox"
-  if !firefox https://www.virtualbox.org/wiki/Linux_Downloads; then
-    echo "firefoy not found on your computer"
-    exit 1
+  if [ "x$(uname -s)" = "xLinux" ]; then
+    echo "you need to download and install VMBox"
+    if !firefox https://www.virtualbox.org/wiki/Linux_Downloads; then
+      echo "firefox not found on your computer"
+      exit 1
+    fi
+  elif [ "x$(uname -s)" = "xDarwin" ]; then
+    wget http://download.virtualbox.org/virtualbox/5.1.20/VirtualBox-5.1.20-114629-OSX.dmg
+    hdiutil attach VirtualBox-5.1.20-114629-OSX.dmg
+    installer -package /Volumes/VirtualBox/VirtualBox.pkg -target /
+    hdiutil detach /Volumes/VirtualBox/
   fi
-elif [ "x$(uname -s)" = "xDarwin" ]; then
- wget http://download.virtualbox.org/virtualbox/5.1.20/VirtualBox-5.1.20-114629-OSX.dmg
- hdiutil attach VirtualBox-5.1.20-114629-OSX.dmg
- installer -package /Volumes/VirtualBox/VirtualBox.pkg -target /
- hdiutil detach /Volumes/VirtualBox/
 fi
- exit 1
-fi
-#dowload the instalaation files from github
+#dowload the installation files from github
 # the install script (to launch the Virtual Machine for MacOS and Linux)
 wget https://raw.githubusercontent.com/yschutz/MasterClass/master/installation/install.sh
 # the configuration files
