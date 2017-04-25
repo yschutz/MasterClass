33fdca261e36572e7d3fd0b2185e4275fc9cb25d
yschutz <schutz@in2p3.fr>
2017-04-25 09:39:02 +0200

    Added a scripr to launch the VM; to be downloaded to your work station

---
+++ installation/startVM.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+if ! virtualbox; then
+ echo "you need to instal VirtualBox first from: https://www.virtualbox.org"
+if [ "x$(uname -s)" = "xLinux" ]; then
+  echo "goto: https://www.virtualbox.org/wiki/Linux_Downloads download and install"
+elif [ "x$(uname -s)" = "xDarwin" ]; then
+ wget http://download.virtualbox.org/virtualbox/5.1.20/VirtualBox-5.1.20-114629-OSX.dmg
+ hdiutil attach VirtualBox-5.1.20-114629-OSX.dmg
+ installer -package /Volumes/VirtualBox/VirtualBox.pkg -target /
+ hdiutil detach /Volumes/VirtualBox/
+fi
+ exit 1
+fi
+#dowload the instalaation files from github
+# the install script (to launch the Virtual Machine for MacOS and Linux)
+wget https://raw.githubusercontent.com/yschutz/MasterClass/master/installation/install.sh
+# the configuration files
+wget https://raw.githubusercontent.com/yschutz/MasterClass/master/installation/CONFIGURATION_FILE
+# the user data file
+wget https://raw.githubusercontent.com/yschutz/MasterClass/master/installation/USER_DATA_FILE
+# make the scrit executable and run it
+chmod +x install.sh
+./install.sh
