cc86acc2505bc37abff3914ed98788d5d8f8f04c
Yves Schutz <schutz@sbgedu231.in2p3.fr>
2017-05-04 16:51:49 +0200

    chaged wget to curl

--- installation/startVM.sh
+++ installation/startVM.sh
@@ -8,7 +8,7 @@ if ! which virtualbox > /dev/null; then
       exit 1
     fi
   elif [ "x$(uname -s)" = "xDarwin" ]; then
-    wget http://download.virtualbox.org/virtualbox/5.1.20/VirtualBox-5.1.20-114629-OSX.dmg
+    curl -o VirtualBox-5.1.20-114629-OSX.dmg http://download.virtualbox.org/virtualbox/5.1.20/VirtualBox-5.1.20-114629-OSX.dmg
     hdiutil attach VirtualBox-5.1.20-114629-OSX.dmg
     installer -package /Volumes/VirtualBox/VirtualBox.pkg -target /
     hdiutil detach /Volumes/VirtualBox/
@@ -16,11 +16,11 @@ if ! which virtualbox > /dev/null; then
 fi
 #dowload the installation files from github
 # the install script (to launch the Virtual Machine for MacOS and Linux)
-wget https://raw.githubusercontent.com/yschutz/MasterClass/master/installation/install.sh
+curl -o install.sh https://raw.githubusercontent.com/yschutz/MasterClass/master/installation/install.sh
 # the configuration files
-wget https://raw.githubusercontent.com/yschutz/MasterClass/master/installation/CONFIGURATION_FILE
+curl -o CONFIGURATION_FILE https://raw.githubusercontent.com/yschutz/MasterClass/master/installation/CONFIGURATION_FILE
 # the user data file
-wget https://raw.githubusercontent.com/yschutz/MasterClass/master/installation/USER_DATA_FILE
+curl -o USER_DATA_FILE https://raw.githubusercontent.com/yschutz/MasterClass/master/installation/USER_DATA_FILE
 # make the scrit executable and run it
 chmod +x install.sh
 ./install.sh
