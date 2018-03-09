2f930e65241052f0c031a718a69d34a50ebe39ca
yves schutz <schutz@in2p3.fr>
2018-03-09 15:30:22 +0100

    Create cernbox.sh
    new script to open the appropriate cernbox dir where to store part 1 results

---
+++ cernbox.sh
@@ -0,0 +1,14 @@
+#
+case $1 in
+linux)
+firefox https://cernbox.cern.ch/index.php/s/zw0cULC2TIkcBMZ
+;;
+windows1)
+firefox https://cernbox.cern.ch/index.php/s/One9UfxlZYkzD9T
+;;
+windows2)
+firefox https://cernbox.cern.ch/index.php/s/FA1cugP86eMRR4G
+;;
+*)
+echo "Usage: cernbox.sh [linux | windows1 | windows2]"
+esac
