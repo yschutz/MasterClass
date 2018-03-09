e357dfd8418ec7f2bdce79a9862c33834fc059c5
yves schutz <schutz@in2p3.fr>
2018-03-09 15:57:26 +0100

    Update cernbox.sh

--- cernbox.sh
+++ cernbox.sh
@@ -1,13 +1,13 @@
 #
 case $1 in
 linux)
-firefox https://cernbox.cern.ch/index.php/s/zw0cULC2TIkcBMZ
+firefox https://cernbox.cern.ch/index.php/s/zw0cULC2TIkcBMZ &
 ;;
 windows1)
-firefox https://cernbox.cern.ch/index.php/s/One9UfxlZYkzD9T
+firefox https://cernbox.cern.ch/index.php/s/One9UfxlZYkzD9T &
 ;;
 windows2)
-firefox https://cernbox.cern.ch/index.php/s/FA1cugP86eMRR4G
+firefox https://cernbox.cern.ch/index.php/s/FA1cugP86eMRR4G &
 ;;
 *)
 echo "Usage: cernbox.sh [linux | windows1 | windows2]"
