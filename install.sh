7064708085b37383382352453181e0f6e1bf350f
schutz <schutz@HomeYS.local>
2019-08-13 15:59:44 +0200

    Improving the install script

--- install.sh
+++ install.sh
@@ -8,12 +8,16 @@ if [ -d $IMCDIR ]; then
 fi    
 #download source code from github into $HOME/MasterClass 
 git clone https://github.com/yschutz/MasterClass.git
+if [ $? -eq 127 ]; then 
+    echo "ERROR: you need to install git on your computer"
+    exit
+fi    
 cd MasterClass 
 #compile and link
 if [ -z ${ROOTSYS+x} ]; then
-echo "ROOTSYS is unset: Install ROOT";
+echo "ROOTSYS is unset: You need to install ROOT";
 echo "Download the appropriate binary distributions from https://root.cern.ch/content/release-61800"
-echo "Make sure gcc and C++ STL are installed"
+echo "Make sure gcc and C++ STL are installed on your computer"
     exit;
 fi;
 LIBDIR=$MCDIR/library
@@ -22,7 +26,7 @@ if [ -d "$LIBDIR" ]; then
 	make
     retVal=$?
  	if [ $retVal -ne 0 ]; then
-    	echo "Compilation Error!"
+    	echo "!!! Compilation Error !!!"
 		cd $MCDIR
 		exit $retVal
 	fi
@@ -32,3 +36,5 @@ else
 	echo "ERROR: $LIBDIR not found!"
 	exit 1
 fi 
+echo "Installation completed .... "
+echo "To start the exercises: ./$MCDIR/MasterClassStart.sh"
