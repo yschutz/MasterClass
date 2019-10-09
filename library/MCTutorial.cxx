9376718abcf054bf90941db29dd6f9cbdae57b60
schutz@in2p3.fr <schutz@sbgmc137.in2p3.fr>
2019-10-09 18:03:01 +0200

    developping tutorial exercise

--- library/MCTutorial.cxx
+++ library/MCTutorial.cxx
@@ -20,6 +20,14 @@ MCTutorial::MCTutorial(const TGWindow *p, UInt_t w, UInt_t h) : MCExercise(p, w,
     gRandom->SetSeed(0); 
     fTimer = new TTimer(); 
     fTimer->Connect("Timeout()", "MCTutorial", this, "DrawTrackSegment()"); 
+
+    fNSteps = 100; 
+    fBz     = 0.5;                    // Tesla
+    fMass   = 130.0 * 1.78E-30;       // kg 
+    fCharge = 1.0 * 1.602E-19;        // Coulomb 
+    fPt     = 200.;                  // MeV 
+
+    fOmega  = fBz * fCharge / fMass; 
     DrawTrack(); 
 }
 
@@ -37,29 +45,34 @@ void MCTutorial::DrawTrack()
     fTrack->SetLineColor(kRed); 
     fTrack->SetLineWidth(3); 
     fTrack->SetLineStyle(1); 
-    fTrackDirection.SetXYZ(1., 0., 0.);
-    fTrackDirection.SetMag(kfMaxRadius/10); 
-    Double_t theta = gRandom->Rndm() * 180.; 
-    Double_t phi   = gRandom->Rndm() * 360.; 
-    fTrackDirection.SetTheta(theta * TMath::DegToRad()); 
-    fTrackDirection.SetPhi(phi * TMath::DegToRad()); 
-    fTrack->SetPoint(0, 0, 0, 0);
+    fTrack->SetSmooth(kTRUE); 
 
+    Double_t theta = 0. ; //gRandom->Rndm() * 180.; 
+    Double_t phi   = 0.; //gRandom->Rndm() * 360.; 
+    fTrackMomentum.SetPtThetaPhi(fPt, theta, phi);  
+    fRadius = fTrackMomentum.Pt() / fBz;
+    std::cout << "px py pz " << fTrackMomentum.Z() << " " << fTrackMomentum.Z() << std::endl;  
+    fTracklet.SetXYZ(fTrackMomentum.X(), fTrackMomentum.Y(), fTrackMomentum.Z());
+    fTracklet.SetMag(kfMaxRadius / fNSteps); 
+
+    fTrack->SetPoint(0, 0, 0, 0); 
     fTimer->Start(300, kFALSE); 
 }
 
 //=MCTutorial========================================================================
 void MCTutorial::DrawTrackSegment()
 {
-    if (fTrack->CalculateLineLength() > kfMaxRadius)
+    Double_t r = TMath::Sqrt(fTrack->GetLineEnd().fX * fTrack->GetLineEnd().fX + fTrack->GetLineEnd().fY * fTrack->GetLineEnd().fY + fTrack->GetLineEnd().fZ * fTrack->GetLineEnd().fZ); 
+    if (r > kfMaxRadius)
     {
         fTimer->Stop(); 
         fTrackSegmentStep = 1; 
+        return; 
     }
-    Float_t x = fTrack->GetLineEnd().fX;   
-    Float_t y = fTrack->GetLineEnd().fY;   
-    Float_t z = fTrack->GetLineEnd().fZ;   
-    fTrack->SetPoint(fTrackSegmentStep, fTrackDirection.X() + x, fTrackDirection.Y() + y, fTrackDirection.Z() + z); 
+    Float_t x, y, z;
+    TrackStep(x, y, z); 
+    fTrack->SetPoint(fTrackSegmentStep, x, y, z); 
+    std::cout << "xyz " << fTrackSegmentStep/100. << " " << x << " " << y << " " << z << std::endl;  
     gEve->AddElement(fTrack);
     TEveElement *top = gEve->GetCurrentEvent();
     if (MCMultiView::Instance().IsSet())
@@ -71,4 +84,22 @@ void MCTutorial::DrawTrackSegment()
     }
     gEve->Redraw3D(kFALSE, kTRUE);
     fTrackSegmentStep++; 
+}
+
+//=MCTutorial========================================================================
+void MCTutorial::TrackStep(Float_t &x, Float_t &y, Float_t &z)
+{
+    if (fBz == 0.0)     
+    {   // linear 
+        x = fTracklet.X() + fTrack->GetLineEnd().fX; 
+        y = fTracklet.Y() + fTrack->GetLineEnd().fY; 
+        z = fTracklet.Z() + fTrack->GetLineEnd().fZ; 
+    }
+    else
+    {
+        x = fRadius * TMath::Sin(fOmega * fTrackSegmentStep / (Double_t)fNSteps);
+        y = fRadius * TMath::Cos(fOmega * fTrackSegmentStep / (Double_t)fNSteps); 
+        z = fTrackMomentum.Z() / (Double_t)fNSteps + fTrack->GetLineEnd().fZ; 
+    }
+    
 }
--- library/MCTutorial.h
+++ library/MCTutorial.h
@@ -11,15 +11,25 @@ class MCTutorial: public MCExercise
 {
     RQ_OBJECT("MCTutorial")
   private:
+    Double_t fBz                {0.0}; 
+    Double_t fCharge; 
+    Double_t fMass;  
+    Int_t    fNSteps; 
+    Double_t fOmega; 
+    Double_t fPt                {2.0}; 
+    Double_t fRadius; 
     TTimer   *fTimer            {nullptr}; 
     TEveLine *fTrack            {nullptr}; 
     Int_t    fTrackSegmentStep  {1}; 
-    TVector3 fTrackDirection; 
+    TVector3 fTracklet; 
+    TVector3 fTrackMomentum; 
+    Double_t fUnitNorm; 
 
     void   CustomizeBrowser(); 
     void   DeleteHistograms(){;} 
     void   DrawTrack();
     void   MakeHistograms(){;} 
+    void   TrackStep(Float_t &x, Float_t &y, Float_t &z); 
 
   public:
     MCTutorial(const TGWindow *p, UInt_t w, UInt_t h);
